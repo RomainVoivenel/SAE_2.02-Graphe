d3ac04e46c8767128ec0b0016bc5fe9a3940dbe4
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-05-31 14:24:02 +0200

    Modification application

--- Graphe/Graphe.py
+++ Graphe/Graphe.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
-from tkinter import *
-from tkinter  import filedialog
 import customtkinter
 
-def json_vers_nx(chemin:str):
+
+from customtkinter import filedialog
+
+def json_vers_nx(chemin:str): #O(n³)
     """Creer une liste de films
 
     Args:
@@ -28,18 +29,6 @@ def json_vers_nx(chemin:str):
                 G.add_edge(cast[i], cast[j])
     
     return G
-
-G = json_vers_nx("Graphe/data.txt")
-
-def dessiner_graph(G:nx.Graph):
-    """Permet d'afficher le graphe
-
-    Args:
-        G (nx.Graph): un graphe
-    """
-    pos = nx.fruchterman_reingold_layout(G, k=2)
-    nx.draw(G, pos=nx.circular_layout(G), with_labels= False,node_size= 50,node_color= "lightgreen",font_size = 10,linewidths = 2)
-    plt.show()
 
 def collaborateurs_communs(G:nx.Graph,u:str,v:str):
     """renvoie l'ensemble des collaborateurs en commun des deux acteurs
@@ -123,7 +112,6 @@ def distance(G:nx.Graph,u:str,v:str):
     """
     return nx.shortest_path_length(G, u, v)
 
-
 # Q4
 
 def centralite(G:nx.Graph,u:str):
@@ -168,8 +156,6 @@ def eloignement_max(G:nx.Graph):
     eccentricity = nx.eccentricity(subgraph)
     return max(eccentricity.values())
 
-print(eloignement_max(G))
-
 # Q bonus
 
 def centralite_groupe(G,S):
@@ -180,15 +166,53 @@ def centralite_groupe(G,S):
 
 # Application
 app = customtkinter.CTk()
-app.geometry("400x150")
+app.geometry("600x250")
+app.title("A la conquête d'Hollywood")
+app.config(background="white")
+
+
+
+
+
+
+# Create a File Explorer label
+label_file_explorer = customtkinter.CTkLabel(app, 
+                            text = "Parcourir un fichier",
+                            width = 100, height = 4)
+  
 
 def parcourir_fichier():
-    filename = filedialog.askopenfilename(initialdir = "/",
+    filename = filedialog.askopenfilename(initialdir = ".",
                                           title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
+                                          filetypes = ((("Text files","*.txt*")),("All files",".")))
+    label_file_explorer.configure(text="File Opened: "+filename)
+    G = json_vers_nx(filename)
+    dessiner_graph(G)
+    
+button_explore = customtkinter.CTkButton(app, text = "Parcourir",command = parcourir_fichier) 
+  
+button_exit = customtkinter.CTkButton(app,text = "Quitter",command = exit)
+  
+button_explore.grid(column = 1, row = 1,padx = 20, pady = 10)
+  
+label_file_explorer.grid(column = 1, row = 2,padx = 20, pady = 5)
+  
+button_exit.grid(column = 1,row = 3,padx = 20, pady = 20)
 
 
+
+def dessiner_graph(G:nx.Graph):
+    """Permet d'afficher le graphe
+
+    Args:
+        G (nx.Graph): un graphe
+    """
+    Gd = nx.Graph(G)
+    data = {(u,v) : d for u,v,d in G.edges.data()}
+    Gdt = nx.dfs_tree(Gd,centre_hollywood(G))
+    nx.set_edge_attributes(Gdt,data)
+    nx.draw(Gdt, pos = nx.planar_layout(Gdt,scale=eloignement_max(Gd)) , with_labels= False,node_size= 50,node_color= "lightgreen",font_size = 10,linewidths = 2)
+    plt.show()
+
+    
 app.mainloop()
